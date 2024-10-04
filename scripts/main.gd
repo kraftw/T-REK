09dd718643a74ca9ee30eca9e0741292466976a1
kraftw <kraftworker12@gmail.com>
2024-10-03 22:06:35 -0400

    Added Strafing Bounds + Level Scrolling

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,16 +1,52 @@
-extends Node2D
+extends Node
+
+
+const PLAYER_START_POS : Vector2i = Vector2i(360, 624)
+const CAMERA_START_POS : Vector2i = Vector2i(960, 540)
+
+var game_speed : float
+const INIT_GAME_SPEED : int = 500
+const MAX_SPEED : int = 2500
+
+var window_size : Vector2i
+var left_strafe_bound : float
+var right_strafe_bound : float
 
 
 func _ready() -> void:
-	pass
+	window_size = get_window().size
+	
+	new_game()
 
 
 func _process(delta: float) -> void:
-	pass
+	move_env(delta)
+	calculate_bounds()
+	maintain_bounds()
 
 
-func reset_game() -> void:
-	pass
+func new_game() -> void:
+	$Player/PlayerSprite.play("idle")
+	$Player.position = PLAYER_START_POS
+	$Player.velocity = Vector2i(0, 0)
+	$Camera2D.position = CAMERA_START_POS
+	$Ground.position = Vector2i(0, 0)
 
-func start_game() -> void:
-	pass
+func move_env(delta: float) -> void:
+	game_speed = INIT_GAME_SPEED
+	
+	$Player.position.x += game_speed * delta
+	$Camera2D.position.x += game_speed * delta
+	
+	if $Camera2D.position.x - $Ground.position.x > window_size.x * 1.5:
+		$Ground.position.x += window_size.x
+
+func maintain_bounds() -> void:
+	if $Player.position.x < left_strafe_bound:
+		$Player.position.x = left_strafe_bound
+	if $Player.position.x > right_strafe_bound:
+		$Player.position.x = right_strafe_bound
+
+func calculate_bounds() -> void:
+	left_strafe_bound = $Camera2D.position.x - (window_size.x * 0.45) # increase to shift left
+	right_strafe_bound = $Camera2D.position.x + (window_size.x * 0.15) # decrease tp shift left
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-# init values
-@export var SPEED : int = 300
-@export var FRICTION : int = 75
-@export var GRAVITY : int = 1200
-@export var JUMP_VELOCITY : int = -800
-@export var MIN_JUMP_VELOCITY : int = -400
-@export var FAST_FALL_ACCELERATION : int = 1200
+
+const STRAFE_SPEED : int = 300
+const FRICTION : int = 75
+const GRAVITY : int = 1200
+const JUMP_VELOCITY : int = -900
+const MIN_JUMP_VELOCITY : int = -600
+const FAST_FALL_ACCELERATION : int = 1200
 
 
 func _physics_process(delta: float) -> void:
@@ -23,21 +23,21 @@ func apply_gravity(delta: float) -> void:
 func apply_horizonatal_movement() -> void:
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * STRAFE_SPEED
 		if is_on_floor():
+			$PlayerSprite.play("run")
+			$RunCol.disabled = false
+			$DuckCol.disabled = true
 			if Input.is_action_pressed("down"):
 				$PlayerSprite.play("duck")
-				$RunCol.hide()
-			else:
-				$PlayerSprite.play("run")
-				$RunCol.show()
+				$RunCol.disabled = true
+				$DuckCol.disabled = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, FRICTION)
-		if velocity.x == 0 and is_on_floor():
-			$PlayerSprite.play("idle")
 
 func apply_vertical_movement(delta: float) -> void:
 	if is_on_floor():
+		$PlayerSprite.play("run")
 		if Input.is_action_pressed("up"):
 			velocity.y = JUMP_VELOCITY
 	else:
