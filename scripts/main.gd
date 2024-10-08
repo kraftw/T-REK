53d1ff0c34132279e4d01cf06c654392dceb9cf5
kraftw <kraftworker12@gmail.com>
2024-10-08 13:32:19 -0400

    Added HUD + Code Cleanup
    TODO: Finish Obstacle Spawning

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,16 +1,40 @@
 extends Node
 
+#region OBSTACLE VARIABLES
+var ground_obs_scene = preload("res://scenes/obstacles/square_ground_obstacle.tscn")
+var flying_enemy_scene = preload("res://scenes/enemies/flying_enemy.tscn")
+var obstacle_types := [ground_obs_scene, flying_enemy_scene]
+var obstacles : Array
+var last_obs : PackedScene
+var flying_enemy_heights := [650, 800]
+#endregion
 
-const PLAYER_START_POS : Vector2i = Vector2i(360, 624)
-const CAMERA_START_POS : Vector2i = Vector2i(960, 540)
+#region STARTING POSITIONS
+const PLAYER_START_POS := Vector2i(360, 624)
+const CAMERA_START_POS := Vector2i(960, 540)
+#endregion
 
+#region SCORE VARIABLES
+var score : int
+const SCORE_MODIFIER : int = 200000
+#endregion
+
+#region SPEED VARIABLES
 var game_speed : float
-const INIT_GAME_SPEED : int = 500
-const MAX_SPEED : int = 2500
+const INIT_GAME_SPEED : float = 500
+const MAX_GAME_SPEED : int = 2500
+const GAME_SPEED_MODIFIER : int = 50000
+#endregion
 
+#region WINDOW VARIABLES
 var window_size : Vector2i
 var left_strafe_bound : float
 var right_strafe_bound : float
+#endregion
+
+#region GAME VARIABLES
+var game_running : bool
+#endregion
 
 
 func _ready() -> void:
@@ -20,27 +44,67 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	move_env(delta)
-	calculate_bounds()
-	maintain_bounds()
+	if game_running:
+		move_env(delta)
+		handle_score()
+		handle_speed()
+		
+		calculate_bounds()
+		maintain_bounds()
+	else:
+		if Input.is_action_pressed("ui_accept"):
+			game_running = true
+			$HUD/StartLabel.hide()
 
 
+#region main_funcs
 func new_game() -> void:
-	$Player/PlayerSprite.play("idle")
+	# RESET VARIABLES
+	score = 0
+	game_running = false
+	
+	# RESET NODES
+	show_score()
+	$HUD/StartLabel.show()
+	$Player/RunCol.disabled = false
+	$Player/DuckCol.disabled = true
 	$Player.position = PLAYER_START_POS
 	$Player.velocity = Vector2i(0, 0)
 	$Camera2D.position = CAMERA_START_POS
 	$Ground.position = Vector2i(0, 0)
 
+#endregion
+
+#region movement_funcs
 func move_env(delta: float) -> void:
 	game_speed = INIT_GAME_SPEED
 	
+	# move dino and camera
 	$Player.position.x += game_speed * delta
 	$Camera2D.position.x += game_speed * delta
 	
+	# update ground position
 	if $Camera2D.position.x - $Ground.position.x > window_size.x * 1.5:
 		$Ground.position.x += window_size.x
 
+func handle_speed() -> void:
+	game_speed = (INIT_GAME_SPEED + score) / GAME_SPEED_MODIFIER
+	if game_speed > MAX_GAME_SPEED:
+		game_speed = MAX_GAME_SPEED
+
+#endregion
+
+#region score_funcs
+func handle_score() -> void:
+	score += game_speed
+	show_score()
+
+func show_score() -> void:
+	$"HUD/Score Labels/ScoreLabel".text = "SCORE: " + str(score / SCORE_MODIFIER)
+
+#endregion
+
+#region bounds_funcs
 func maintain_bounds() -> void:
 	if $Player.position.x < left_strafe_bound:
 		$Player.position.x = left_strafe_bound
@@ -50,3 +114,5 @@ func maintain_bounds() -> void:
 func calculate_bounds() -> void:
 	left_strafe_bound = $Camera2D.position.x - (window_size.x * 0.45) # increase to shift left
 	right_strafe_bound = $Camera2D.position.x + (window_size.x * 0.15) # decrease tp shift left
+
+#endregion
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,20 @@
 extends CharacterBody2D
 
-
+#region PLAYER VARIABLES
 const STRAFE_SPEED : int = 300
 const FRICTION : int = 75
 const GRAVITY : int = 1200
 const JUMP_VELOCITY : int = -900
 const MIN_JUMP_VELOCITY : int = -600
 const FAST_FALL_ACCELERATION : int = 1200
+#endregion
 
 
 func _physics_process(delta: float) -> void:
 	apply_gravity(delta)
 	apply_vertical_movement(delta)
 	apply_horizonatal_movement()
+	handle_animation()
 
 	move_and_slide()
 
@@ -22,32 +24,48 @@ func apply_gravity(delta: float) -> void:
 
 func apply_horizonatal_movement() -> void:
 	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * STRAFE_SPEED
+	if get_parent().game_running:
 		if is_on_floor():
-			$PlayerSprite.play("run")
-			$RunCol.disabled = false
-			$DuckCol.disabled = true
+			# ACCELERATE
+			if direction:
+				velocity.x = direction * STRAFE_SPEED
+			# DECELERATE
+			else:
+				velocity.x = move_toward(velocity.x, 0, FRICTION)
+
+func apply_vertical_movement(delta: float) -> void:
+	if get_parent().game_running:
+		# BEGIN JUMP
+		if is_on_floor() and Input.is_action_pressed("up"):
+			velocity.y = JUMP_VELOCITY
+		else:
+			# SHORT HOP
+			if Input.is_action_just_released("up") and velocity.y < MIN_JUMP_VELOCITY:
+				velocity.y = MIN_JUMP_VELOCITY
+			# FAST FALL
+			if Input.is_action_pressed("down"):
+				velocity.y += FAST_FALL_ACCELERATION * delta
+
+func handle_animation() -> void:
+	# IDLE TRIGGER
+	if not get_parent().game_running:
+		$PlayerSprite.play("idle")
+	else:
+		if is_on_floor():
+			# DUCK TRIGGER
 			if Input.is_action_pressed("down"):
 				$PlayerSprite.play("duck")
 				$RunCol.disabled = true
 				$DuckCol.disabled = false
-	else:
-		velocity.x = move_toward(velocity.x, 0, FRICTION)
-
-func apply_vertical_movement(delta: float) -> void:
-	if is_on_floor():
-		$PlayerSprite.play("run")
-		if Input.is_action_pressed("up"):
-			velocity.y = JUMP_VELOCITY
-	else:
-		if Input.is_action_just_released("up") and velocity.y < MIN_JUMP_VELOCITY:
-			velocity.y = MIN_JUMP_VELOCITY
-			
-		if Input.is_action_pressed("down"):
-			velocity.y += FAST_FALL_ACCELERATION * delta
-		
-		if velocity.y < 0:
-			$PlayerSprite.play("jump")
+			# RUN TRIGGER
+			else:
+				$PlayerSprite.play("run")
+				$RunCol.disabled = false
+				$DuckCol.disabled = true
 		else:
-			$PlayerSprite.play("fall")
+			# JUMP TRIGGER
+			if velocity.y < 0:
+				$PlayerSprite.play("jump")
+			# FALL TRIGGER
+			else:
+				$PlayerSprite.play("fall")
